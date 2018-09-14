e211d41d5ddc5648bdee1a2b10393f3be9322a3b
NarcisM <narcismorar@gmail.com>
2018-09-14 19:43:26 +0200

    push

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$subject = 'Contact us';
+
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+//below is what the customer types on the message
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$msg .='Email from '.$email.'
+Subject from '.$subject.'
+    Email from '.$email.'';		
+
+mail("joeljfernandes@yahoo.com",$subject,$msg,$headers);
+
+?>
